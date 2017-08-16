44f89f885f1a4c264520572517378b7fa571a49f
Clifford Beta <betaclifford@gmail.com>
2017-08-16 22:35:45 +0300

    fixed test cases for message store
    Signed-off-by: Clifford Beta <betaclifford@gmail.com>

--- store/sql_message_store_test.go
+++ store/sql_message_store_test.go
@@ -6,6 +6,7 @@ import (
 	"salv_prj/model"
 )
 
+var msgStore = SqlMessageStore{Database}
 func TestSqlMessageStore_CreateMessage(t *testing.T) {
 	msg := model.Message{
 		Id:1,
@@ -18,5 +19,24 @@ func TestSqlMessageStore_CreateMessage(t *testing.T) {
 		TimeStamp:time.Now(),
 		DateSent:time.Now(),
 	}
+	res := <- msgStore.CreateMessage(&msg)
+	if res.Err != nil {
+		t.Errorf("CreateMessage Failed with",res.Err)
+	}
 
+}
+
+func TestSqlMessageStore_RetrieveMessage(t *testing.T) {
+	res := <- msgStore.RetrieveMessage(1)
+	if res.Err != nil {
+		t.Errorf("RetrieveMessage Failed with",res.Err)
+	}
+}
+
+func TestSqlMessageStore_RetrieveAll(t *testing.T) {
+	res := <- msgStore.RetrieveAll()
+	if res.Err != nil {
+		t.Errorf("RetrieveAllMessages Failed with",res.Err)
+
+	}
 }
