cfcc51d3fe1633731906e1418523c2e33f2b228a
Clifford Beta <betaclifford@gmail.com>
2017-08-17 11:02:41 +0300

    Done with file store
    Signed-off-by: Clifford Beta <betaclifford@gmail.com>

---
+++ store/sql_file_store_test.go
@@ -0,0 +1,72 @@
+package store
+
+import (
+	"testing"
+	"time"
+	"salv_prj/model"
+)
+
+var fileStore = SqlFileStore{Database}
+
+
+func TestSqlFileStore_CreateFileType(t *testing.T) {
+	fltp := model.FileType{
+		Name:"Memo",
+		Description:"These are memos",
+		Store:"Memoir",
+		TimeStamp:time.Now(),
+
+	}
+	res := <- fileStore.CreateFileType(&fltp)
+	if res.Err != nil {
+		t.Errorf("Create File Type Failed with",res.Err)
+	}
+}
+
+func TestSqlFileStore_Create(t *testing.T) {
+	inf := model.File{
+		Name:"Computer",
+		Type:1,
+		Description:"Desktop computers",
+		DateCreated:time.Now(),
+		TimeStamp:time.Now(),
+
+	}
+	res := <- fileStore.Create(&inf)
+	if res.Err != nil {
+		t.Errorf("Create File Failed with",res.Err)
+	}
+
+}
+
+
+
+func TestSqlFileStore_RetrieveOne(t *testing.T) {
+	res := <- fileStore.RetrieveOne(1)
+	if res.Err != nil {
+		t.Errorf("Retrieve File Failed with",res.Err)
+	}
+}
+
+func TestSqlFileStore_RetrieveOneType(t *testing.T) {
+	res := <- fileStore.RetrieveOneType(1)
+	if res.Err != nil {
+		t.Errorf("Retrieve File Type Failed with",res.Err)
+	}
+}
+
+func TestSqlFileStore_RetrieveAll(t *testing.T) {
+	res := <- fileStore.RetrieveAll()
+	if res.Err != nil {
+		t.Errorf("RetrieveAll Failed with",res.Err)
+
+	}
+}
+
+func TestSqlFileStore_RetrieveAllTypes(t *testing.T) {
+	res := <- fileStore.RetrieveAllTypes()
+	if res.Err != nil {
+		t.Errorf("RetrieveAllTypes Failed with",res.Err)
+
+	}
+}
